5c795f3f2cf33aff70fe48b7bc7cc8280fbbe4a2
Felipe <feiandres57@gmail.com>
2025-08-31 21:39:57 -0300

    MateriasInscription-service

--- auth-service/src/main/java/com/app/auth_service/domain/port/out/JwtServicePort.java
+++
@@ -1,8 +0,0 @@
-package com.app.auth_service.domain.port.out;
-
-import com.app.auth_service.infrastructure.adapter.in.DTOS.UserRequest;
-
-public interface JwtServicePort {
-
-    String GenerateToken(UserRequest request);
-}
--- carreras-service/src/main/java/com/app/carreras_service/infrastructure/persistance/repository/CarreraRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.carreras_service.infrastructure.persistance.repository;
-
-import com.app.carreras_service.infrastructure.persistance.entity.CarreraEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarreraRepository extends JpaRepository<CarreraEntity,Long> {
-}
